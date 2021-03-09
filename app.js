d38df4e8c98046bbde9082488882fe5b1a334e68
pthomas2001 <71702126+pthomas2001@users.noreply.github.com>
2021-03-09 09:16:08 +0100

    canvas.width, canvas.height

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d'); //rajzolási kontextus (lehet 3d is)
 context.fillStyle = 'red'; //alakzat kitöltőszíne
 context.fillRect(175,100,100,100); //négyszög rajzolása (bal felső sarok x,y koordinátája, szélesség, magasság)
+
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
+
+context.fillRect(50,50,canvasWidth-100,canvasHeight-100);
